b3dd0679bd895f21769547216d098b9808ca2805
Durim Jusaj <durim.jusaj@orcaya.com>
2024-03-07 11:10:32 +0100

    - add symfony 7.0 support

--- Job/ShellJobWrapper.php
+++ Job/ShellJobWrapper.php
@@ -4,8 +4,19 @@ namespace Cron\CronBundle\Job;
 
 use Cron\CronBundle\Entity\CronJob;
 use Cron\Job\ShellJob;
+use Symfony\Component\Process\Process;
 
 class ShellJobWrapper extends ShellJob
 {
     public ?CronJob $raw = null;
+
+    public function setCommand($command)
+    {
+        if (method_exists(Process::class, 'fromShellCommandline')) {
+            $this->process = Process::fromShellCommandline($command);
+        } else {
+            $commandArray = explode(' ', $command);
+            $this->process = new Process($commandArray);
+        }
+    }
 }
